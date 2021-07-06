aedaf43df363596957f6f37eba8a334029139c93
pratik.mondal <pmondal2424@gmail.com>
2021-07-06 20:23:13 +0600

    minor fixing

--- src/main/java/com/example/weTraveller/entity/User.java
+++ src/main/java/com/example/weTraveller/entity/User.java
@@ -1,13 +1,13 @@
 package com.example.weTraveller.entity;
 
-import com.example.weTraveller.entity.common.CommonColumn;
+
 import com.example.weTraveller.model.UserModel;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-public class User extends CommonColumn {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
