ca592994bb2235d15d185fb5f488c120541f4c69
pratik.mondal <pmondal2424@gmail.com>
2021-07-06 07:43:59 +0600

    spring-boot environment with spring-web, theymleaf, mysql, jdbc dependencies

---
+++ src/main/java/com/example/weTraveller/WeTravellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weTraveller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeTravellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeTravellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weTraveller/WeTravellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weTraveller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeTravellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
